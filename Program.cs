9bb8b33882ec871eb984c9712abc347de10c4bf8
Pern Lundell <pernlundell94@gmail.com>
2023-03-05 01:17:31 -0500

    Added choices

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hi Jesse, how are you today?");
+
+Console.WriteLine("(A) - Good");
+Console.WriteLine("(B) - Bad");
+Console.WriteLine("(C) - Idk");
