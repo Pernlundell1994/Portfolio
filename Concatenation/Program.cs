7c754732bb7eaa0f64b9ec319a827e92fa96bd5e
Pern Lundell <pernlundell94@gmail.com>
2023-03-20 19:35:51 -0400

    added Concatenation program

---
+++ Concatenation/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string firstSymbol = "$$";
+string lastSymbol = "USD";
+int benjamin = 100;
+string wallet = string.Concat(firstSymbol + " " + benjamin + " " + lastSymbol); //why did the plus sign work but not comma?
+Console.WriteLine(wallet);
