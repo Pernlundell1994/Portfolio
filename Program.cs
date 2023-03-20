f8c9a2861e8a9d52ea424823bf2407e29dc25b97
Pern Lundell <pernlundell94@gmail.com>
2023-03-20 18:33:11 -0400

    added new function to seaparate choice loop

--- Program.cs
+++ Program.cs
@@ -1,7 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi Jesse, how are you today?");
+using System;
 
-Console.WriteLine("(A) - Good");
-Console.WriteLine("(B) - Bad");
-Console.WriteLine("(C) - Idk");
+class Program{
+
+    static public void Main(){
+        Console.WriteLine("Hi Jesse, how are you today?");
+
+        Console.WriteLine("(A) - Good");
+        Console.WriteLine("(B) - Bad");
+        Console.WriteLine("(C) - Idk");
+        Console.WriteLine();
+        Console.ReadLine();
+
+        Program.choiceLoop();
+    }
+
+    static private void choiceLoop(){
+        while(true){
+            string choice = "";
+            if (choice == "A"){
+                Console.WriteLine("Yeah Man!");
+            }
+            else if (choice == "B"){
+                Console.WriteLine("Not Good Man");
+            }
+            else if (choice == "C"){
+                Console.WriteLine("Idk Man");
+            }
+        }
+    }
+}
