3c8025ef821b901b11003a9f620523307d9a39f5
Pern Lundell <pernlundell94@gmail.com>
2023-03-21 21:28:25 -0400

    added switch statetement, function to call back

--- Program.cs
+++ Program.cs
@@ -1,32 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+namespace Protfolio;
+static class Program
+{
 
-class Program{
+    static void Main(){
+        ShowConcat();
+        //SelectChoice();
+    }
 
-    static public void Main(){
+    static private void SelectChoice()
+    {
         Console.WriteLine("Hi Jesse, how are you today?");
 
         Console.WriteLine("(A) - Good");
         Console.WriteLine("(B) - Bad");
         Console.WriteLine("(C) - Idk");
         Console.WriteLine();
-        Console.ReadLine();
+        var choice = Console.ReadLine(); //dfference between function and procedure
 
-        Program.choiceLoop();
+        HandleChoice(choice);
     }
-
-    static private void choiceLoop(){
-        while(true){
-            string choice = "";
-            if (choice == "A"){
+    static private void HandleChoice(string? choice) //can initialize variable within parameters to be returned
+    {                                               //how to clear text on console app
+        switch (choice)
+        {
+            case "A":
                 Console.WriteLine("Yeah Man!");
-            }
-            else if (choice == "B"){
+                break;
+            case "B":
                 Console.WriteLine("Not Good Man");
-            }
-            else if (choice == "C"){
+                break;
+            case "C":
                 Console.WriteLine("Idk Man");
-            }
-        }
+                break;
+            default:
+                Console.WriteLine("Please input a correct reponse.");
+                SelectChoice();
+                    break;
+        }   
+    }
+    static private void ShowConcat()
+    {
+        string firstSymbol = "$$";
+        string lastSymbol = "USD";
+        int benjamin = 100;
+        string wallet = string.Concat(firstSymbol, " ", benjamin, " ", lastSymbol); //why did the plus sign work but not comma?
+        Console.WriteLine(wallet);
     }
 }
